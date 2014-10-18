f44e0859f38000816e86a01f64325cc6b6b82b22
kirisanthsubramaniam <kirisanth.s@gmail.com>
2014-10-18 03:06:27 -0400

    Displays hardcoded value

--- app/src/main/java/com/example/kirisanthsubramaniam/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/kirisanthsubramaniam/funfacts/FunFactsActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,6 +15,20 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+
+        //Declare our View variables and assign the Views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener(){
+            public void onClick(View view){
+                String fact = "Ostriches can run faster than horses";
+                //This is a closure requires factLabel to be final
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
 
